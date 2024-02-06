5b64436985da23b30d86df61620f56795db58563
K-Wu <kunwu2@illinois.edu>
2024-02-06 13:02:07 -0600

    add dummy server test

---
+++ benchmark/playground/dummy_client.py
@@ -0,0 +1,14 @@
+import socket
+
+def connect_to_dummy_server(host='127.0.0.1', port=12345):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print(f"Connected to {host}:{port}")
+        data = s.recv(1024)
+        print(f"Received: {data.decode()}")
+
+if __name__ == '__main__':
+    import sys
+    print("sys.argv: ", sys.argv)
+    print("Using ip: ", sys.argv[1])
+    connect_to_dummy_server(host = sys.argv[1])
